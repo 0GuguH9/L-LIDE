74a183b5542d51f9a060d35f907e1dd4f6a73768
0GuguH9 <ghstavohbardinelli@gmail.com>
2024-07-18 13:21:48 -0300

    highlight prototype

--- include/core/theme.hpp
+++ include/core/theme.hpp
@@ -5,19 +5,32 @@
 #include <unordered_map>
 
 namespace LLIDE::Core {
+
+    const std::string defaultColor = "#FFFFFF";
+
+    struct TextStyle {
+        std::string hexColor;
+        bool bold = false;
+        bool strikethrough = false;
+        bool underline = false;
+        bool resetOnFalse = false;
+    };
+
+    const TextStyle defaultTextStyle = {defaultColor, false, false, false};
+
     struct ThemeToken {
         std::string id;
-        std::string hexColor;
+        TextStyle style;
     };
 
     class Theme {
         private:
-            std::unordered_map<std::string, std::string> themeTokens;
+            std::unordered_map<std::string, TextStyle> themeTokens;
 
         public:
-            void addThemeToken(std::string id, std::string hexColor);
-            void removeThemeToken(std::string id);
-            ThemeToken getThemeToken(std::string id);
+            void addThemeToken(const std::string id, const TextStyle style = defaultTextStyle);
+            void removeThemeToken(const std::string id);
+            ThemeToken getThemeToken(const std::string id);
     };
 }
 
--- src/core/code_viewer.cpp
+++ src/core/code_viewer.cpp
@@ -1,5 +1,11 @@
 #include "core/code_viewer.hpp"
 
+#include <gtkmm/texttag.h>
+#include <pangomm/attributes.h>
+#include <pangomm/fontdescription.h>
+
+#include <regex>
+
 using namespace LLIDE::Core;
 
 CodeViewer::CodeViewer() {
@@ -16,4 +22,56 @@ CodeViewer::CodeViewer() {
     viewer.set_margin_top(56);
 
     viewer.set_monospace(true);
+}
+
+void CodeViewer::highlight(const std::string regex, const TextStyle style) {
+
+    Glib::RefPtr<Gtk::TextBuffer> buffer = viewer.get_buffer();
+
+    std::string text = buffer->get_text();
+
+    Glib::RefPtr<Gtk::TextTag> keywordTag = Gtk::TextBuffer::Tag::create();
+
+    keywordTag->property_foreground() = style.hexColor;
+    if (style.strikethrough)
+        keywordTag->property_strikethrough() = style.strikethrough;
+    else if (style.resetOnFalse)
+        keywordTag->property_strikethrough() = style.strikethrough;
+
+    if (style.bold)
+        keywordTag->property_weight() = Pango::Weight::BOLD;
+    else if (style.resetOnFalse)
+        keywordTag->property_weight() = Pango::Weight::NORMAL;
+
+    if (style.underline)
+        keywordTag->property_underline() = Pango::Underline::SINGLE;
+    else if (style.resetOnFalse)
+        keywordTag->property_underline() = Pango::Underline::NONE;
+
+    buffer->get_tag_table()->add(keywordTag);
+
+    std::regex regexEpr(regex);
+
+    Gtk::TextBuffer::iterator start, end;
+    start = buffer->begin();
+    end = buffer->end();
+
+    std::smatch match;
+
+    int offset = 0;
+    
+    while (std::regex_search(text, match, regexEpr)) {
+
+        Gtk::TextBuffer::iterator match_start = start;
+        match_start.forward_chars(match.position() + offset);
+
+        Gtk::TextBuffer::iterator match_end = match_start;
+        match_end.forward_chars(match.length());
+
+        buffer->apply_tag(keywordTag, match_start, match_end);
+
+        offset += text.length() - match.suffix().str().length();
+        
+        text = match.suffix().str();
+    }
 }
